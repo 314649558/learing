3a18d343aa5b95ed1c3565ab884d03fb9942ddb1
314649558 <314649558@qq.com>
2018-11-11 19:47:53 +0800

    111

--- bigdata_parent/bigdata_netty/src/main/java/com/bigdata/bdp/netty/quanweizhinan/NettyConstants.java
+++ bigdata_parent/bigdata_netty/src/main/java/com/bigdata/bdp/netty/quanweizhinan/NettyConstants.java
@@ -9,4 +9,12 @@ public class NettyConstants {
     public static final String DELIMITER="$_";
 
     public static final int PORT=8080;
+
+    public static final String REMOTE_HOST="192.168.112.100";
+
+    public static final String LOCAL_HOST="127.0.0.1";
+
+    public static final int BACKLOG_SIZE=1024;
+
+
 }
